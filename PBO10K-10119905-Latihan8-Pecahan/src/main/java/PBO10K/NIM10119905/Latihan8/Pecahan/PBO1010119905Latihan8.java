a7037d246893c5ad5dc57e8afedac7c6ae308832
arumayara <72974847+arumayara@users.noreply.github.com>
2020-10-19 07:15:19 +0700

    Add files via upload

---
+++ PBO10K-10119905-Latihan8-Pecahan/src/main/java/PBO10K/NIM10119905/Latihan8/Pecahan/PBO1010119905Latihan8.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBO10K.NIM10119905.Latihan8.Pecahan;
+
+/**
+ *
+ *  @author 
+ * NAMA    : Abraham Rumayara
+ * KELAS   : PBO10K
+ * NIM     : 10119905
+ * Deskripsi Program : Program ini menampilkan tipe data pecahan
+ * 
+ */
+public class PBO1010119905Latihan8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("float :"+f);
+        System.out.println("Double :"+d);
+    }
+    
+}
